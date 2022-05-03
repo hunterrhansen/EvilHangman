c0b43501f498413bc00d8a90de3270ee96544fd9
Hunter Hansen <hunterhansen50@gmail.com>
2022-05-03 14:23:54 -0600

    startGame and exceptions

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -1,8 +1,25 @@
 package hangman;
 
+import java.io.File;
+import java.io.IOException;
+
 public class EvilHangman {
 
     public static void main(String[] args) {
+        File dictionary = new File(args[0]);
+        int wordLength = Integer.parseInt(args[1]);
+        int numGuesses = Integer.parseInt(args[2]);
+
+        EvilHangmanGame evilHangmanGame = new EvilHangmanGame();
+
+        try {
+            evilHangmanGame.startGame(dictionary, wordLength);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (EmptyDictionaryException e) {
+            System.out.println(e.getMessage());
+        }
+
 
     }
 
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -1,14 +1,42 @@
 package hangman;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
+  private Set<String> possibleWords;
+
+  public EvilHangmanGame() {
+    possibleWords = new TreeSet<>();
+  }
+
   @Override
   public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
+    try (Scanner sc = new Scanner(dictionary)) {
+      if (!sc.hasNext()) {
+        throw new EmptyDictionaryException("The provided dictionary file is empty.");
+      }
 
+      while (sc.hasNext()) {
+        String nextWord = sc.next();
+        if (nextWord.length() == wordLength) {
+          possibleWords.add(nextWord);
+        }
+      }
+    } catch (FileNotFoundException ex) {
+      System.out.println("Could not find file " + dictionary);
+      ex.printStackTrace();
+    }
+
+
+    if (possibleWords.isEmpty()) {
+      throw new EmptyDictionaryException("The provided dictionary file did not contain any words of length " + wordLength);
+    }
   }
 
   @Override
