7207fa8270691c433cafb07c13fa0baba23b7886
Hunter Hansen <hunterhansen50@gmail.com>
2022-05-05 10:50:02 -0600

    implement prioritization

--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -3,10 +3,7 @@ package hangman;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
   private Set<String> possibleWords;
@@ -51,6 +48,7 @@ public class EvilHangmanGame implements IEvilHangmanGame {
 
   @Override
   public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
+    // check if guess is valid and add it to the guessed letters array
     guess = Character.toLowerCase(guess);
     if (guessedLetters.contains(guess)) {
       throw new GuessAlreadyMadeException("Guess already made!");
@@ -61,7 +59,115 @@ public class EvilHangmanGame implements IEvilHangmanGame {
       guessedLetters.add(guess);
     }
 
+    // initialize variables
+    Map<String, Set<String>> partitions = new HashMap<>(); // keeps track of the various partitions and size of each
+    int maxPartitionSize = 0; // keeps track of the biggest partition
+    int minLetterOccurrences = wordPattern.length(); // keeps track of the smallest number of times a letter occurs in the word
+
+    // iterate through each word in current set of possible words and add them to the proper partition
+    for (String word : possibleWords) {
+      int currOccurrences = 0;
+      StringBuilder pattern = new StringBuilder();
+
+      // generating key
+      for (int i = 0; i < word.length(); i++) {
+        if (word.charAt(i) == guess) {
+          pattern.append(guess);
+          currOccurrences++;
+        } else {
+          pattern.append(wordPattern.charAt(i));
+        }
+      }
+      String key = pattern.toString();
+
+      // generate partitons and add word into respective partition
+      if (!partitions.containsKey(key)) {
+        Set<String> keySet = new HashSet<>();
+        keySet.add(word);
+        partitions.put(key, keySet);
+      } else {
+        partitions.get(key).add(word);
+      }
+
+      if (partitions.get(key).size() > maxPartitionSize) {
+        maxPartitionSize = partitions.get(key).size();
+      }
+      if (currOccurrences < minLetterOccurrences) {
+        minLetterOccurrences = currOccurrences;
+      }
+    }
+
+    prioritizeLargestSet(partitions, maxPartitionSize);
+    if (partitions.size() > 1) {
+      for (String pattern : partitions.keySet()) {
+        if (!pattern.contains(Character.toString(guess))) {
+          letterOccurrences = 0;
+          possibleWords = partitions.get(pattern);
+          return possibleWords;
+        }
+      }
+    }
+
+    prioritizeFewestOccurrences(partitions, minLetterOccurrences, guess);
+    if (partitions.size() > 1) {
+      prioritizeRightMost(partitions, wordPattern.length() - 1, guess);
+    }
+
+    String finalPattern = "";
+    int occurrences = 0;
+    for (String pattern : partitions.keySet()) {
+      for (int i = 0; i < pattern.length(); i++) {
+        if (pattern.charAt(i) == guess) {
+          occurrences++;
+        }
+      }
+      finalPattern = pattern;
+    }
+
+    wordPattern = finalPattern;
+    letterOccurrences = occurrences;
+    possibleWords = partitions.get(finalPattern);
     return possibleWords;
+  }
+
+  private void prioritizeLargestSet(Map<String, Set<String>> partitions, int maxSize) {
+    partitions.values().removeIf(set -> set.size() < maxSize);
+  }
+
+  private void prioritizeFewestOccurrences(Map<String, Set<String>> partitions, int minOccurrences, char guess) {
+    for (Iterator<Map.Entry<String, Set<String>>> itr = partitions.entrySet().iterator(); itr.hasNext();) {
+      int occurrences = 0;
+      String pattern = itr.next().getKey();
+      for (int i = 0; i < pattern.length(); i++) {
+        if (pattern.charAt(i) == guess) {
+          occurrences++;
+        }
+      }
+      if (occurrences > minOccurrences) {
+        itr.remove();
+      }
+    }
+  }
+
+  private void prioritizeRightMost(Map<String, Set<String>> partitions, int startIndex, char guess) {
+    int ind = 0;
+
+    for (String pattern : partitions.keySet()) {
+      int i = startIndex;
+      while (i > ind) {
+        if (pattern.charAt(i) == guess) {
+          ind = i;
+        }
+        i--;
+      }
+    }
+
+    int rightmostInd = ind;
+    partitions.keySet().removeIf(key -> key.charAt(rightmostInd) != guess);
+
+    if (partitions.size() > 1) {
+      prioritizeRightMost(partitions, rightmostInd - 1, guess);
+    }
   }
 
   @Override
